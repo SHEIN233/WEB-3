36c3732d4ca210c94ef64443a01bf2c37c244b71
SHEIN233 <sheinalan7@gmail.com>
2025-09-24 10:15:57 -0400

    Update ejercicio1.js

--- practica-1/ejercicio1.js
+++ practica-1/ejercicio1.js
@@ -1,1 +1,18 @@
+function miFuncion(texto) {
 
+  const vocales = { a: 0, e: 0, i: 0, o: 0, u: 0 };
+  
+  texto = texto.toLowerCase();
+ 
+  for (let letra of texto) {
+    
+    if (vocales[letra] !== undefined) {
+      vocales[letra]++;
+    }
+  }
+
+  return vocales;
+}
+
+let obj = miFuncion("euforia");
+console.log(obj); 
