a8e4e790f8e4613e1205a4136fcea13af1117b4e
SHEIN233 <sheinalan7@gmail.com>
2025-09-24 10:19:14 -0400

    Create ejercicio10.js

---
+++ practica-1/ejercicio10.js
@@ -0,0 +1,10 @@
+// CUANDO USAR CALLBACKS
+// Los callbacks son útiles cuando tenemos operaciones sencillas y rápidas,
+// especialmente si solo necesitamos ejecutar algo después de otra función.
+// Pero, si se encadenan muchos callbacks, aparece el problema del
+// "Callback Hell".
+
+// CUANDO USAR PROMESAS
+// Las promesas son más convenientes cuando manejamos operaciones
+// asíncronas más complejas, o varias en cadena.
+// Son más legibles y fáciles de manejar con .then(), .catch() o async/await.
